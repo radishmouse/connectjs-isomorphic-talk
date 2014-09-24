7465fdde7737e36a31e7334adca9f82490242878
Chris Aquino <caquino@bignerdranch.com>
2014-09-24 17:15:55 -0400

    added doctype

--- demos/02.shared-components/index.js
+++ demos/02.shared-components/index.js
@@ -12,7 +12,7 @@ require('node-jsx').install();
 var helloWorld = require('./components/hello-world.js');
 
 app.get('/', function (req, res) {
-    res.send('<html><head></head><body>' + React.renderComponentToString(helloWorld()) + '<script src="scripts/bundle.js"></script></body></html>');
+    res.send('<!doctype html><html><head></head><body>' + React.renderComponentToString(helloWorld()) + '<script src="scripts/bundle.js"></script></body></html>');
 });
 app.use('/scripts', express.static(path.join(__dirname, 'static/scripts')))
 // app.get('/goodbye', function (req, res) {
